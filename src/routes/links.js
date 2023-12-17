a7c9e1659efa54ab0adc698a71e7c36d4b2c979f
Alex Ernesto Salazar Parra <alexernestosp@gmail.com>
2023-12-17 08:53:04 -0500

    Diseño y funcionamiento de la web

--- src/routes/links.js
+++ src/routes/links.js
@@ -16,6 +16,16 @@ router.post("/add", async (req, res) => {
     user_id: 1,
   };
   await pool.query("INSERT INTO links SET ?", [newLink]);
-  res.send("Received");
+  res.redirect("/links");
 });
-module.exports = router;
+
+router.get("/", async (req, res) => {
+  const links = await pool.query("SELECT * FROM links");
+  res.render("links/list", { links });
+});
+
+router.get("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  await pool.query("DELETE FROM links WHERE ID = ?", [id]);
+  res.redirect("/links");
+});
