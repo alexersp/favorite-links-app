599c04653853ccae19ae1f68ba3b6fe4b09b5c9f
Alex Ernesto Salazar Parra <alexernestosp@gmail.com>
2023-12-17 09:43:50 -0500

    Edit de los links funcionando correctamente

--- src/routes/links.js
+++ src/routes/links.js
@@ -29,3 +29,24 @@ router.get("/delete/:id", async (req, res) => {
   await pool.query("DELETE FROM links WHERE ID = ?", [id]);
   res.redirect("/links");
 });
+
+router.get("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const link = await pool.query("SELECT * FROM links WHERE id = ?", [id]);
+
+  res.render("links/edit", { link: link[0] });
+});
+
+router.post("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, description, url } = req.body;
+  const newLink = {
+    title,
+    description,
+    url,
+  };
+  await pool.query("UPDATE links set ? WHERE id = ?", [newLink, id]);
+  res.redirect("/links");
+});
+
+module.exports = router;
